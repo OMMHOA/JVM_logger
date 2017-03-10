5d7a055c3c4a78b122af5fb6ceac23d8915a17d2
ommhoa <omar.osman.asd@gmail.com>
2017-03-10 16:16:07 +0100

    first commit

---
+++ receiver/src/main/java/Entity/Message.java
@@ -0,0 +1,32 @@
+package Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "message")
+public class Message {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String message;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ receiver/src/main/java/main/Recv.java
@@ -0,0 +1,36 @@
+package main;
+
+import Entity.Message;
+import com.rabbitmq.client.*;
+import dal.MessageFacade;
+
+import java.io.IOException;
+
+public class Recv {
+
+  private final static String QUEUE_NAME = "hello";
+  private final static MessageFacade messageFacade = new MessageFacade();
+
+  public static void main(String[] argv) throws Exception {
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("localhost");
+    Connection connection = factory.newConnection();
+    Channel channel = connection.createChannel();
+
+    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+    Consumer consumer = new DefaultConsumer(channel) {
+      @Override
+      public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+          throws IOException {
+        String message = new String(body, "UTF-8");
+        System.out.println(" [x] Received '" + message + "'");
+        Message messageEntity = new Message();
+        messageEntity.setMessage(message);
+        messageFacade.create(messageEntity);
+      }
+    };
+    channel.basicConsume(QUEUE_NAME, true, consumer);
+  }
+}
