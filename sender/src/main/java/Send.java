cce7c0206d9affdd16b4397a3211d4dee68295e5
ommhoa <omar.osman.asd@gmail.com>
2017-03-11 09:31:16 +0100

    Send basic HTTP request

--- sender/src/main/java/Send.java
+++ sender/src/main/java/Send.java
@@ -1,23 +1,27 @@
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import org.apache.http.HttpVersion;
+import org.apache.http.message.BasicHttpRequest;
 
 public class Send {
 
-  private final static String QUEUE_NAME = "hello";
+    private final static String QUEUE_NAME = "hello";
 
-  public static void main(String[] argv) throws Exception {
-    ConnectionFactory factory = new ConnectionFactory();
-    factory.setHost("localhost");
-    Connection connection = factory.newConnection();
-    Channel channel = connection.createChannel();
+    public static void main(String[] argv) throws Exception {
+        BasicHttpRequest request = new BasicHttpRequest("GET", "/", HttpVersion.HTTP_1_1);
 
-    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-    String message = "Hello World!";
-    channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
-    System.out.println(" [x] Sent '" + message + "'");
+        String message = request.toString();
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
 
-    channel.close();
-    connection.close();
-  }
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+        System.out.println(message);
+
+        channel.close();
+        connection.close();
+    }
 }
